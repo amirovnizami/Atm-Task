272acafa466f58fbe9aaabaa1828eb9de4ed056c
Nizami Amirov <amirovnizami395@gmail.com>
2024-06-29 23:41:04 +0400

    Serialization

---
+++ JsonHomework/Car.cs
@@ -0,0 +1,63 @@
+﻿namespace JsonHomework;
+
+public class Car
+{
+    public Car(int id, string model, string marka, int year)
+    {
+        Id = id;
+        Model = model;
+        Marka = marka;
+        Year = year;
+    }
+
+    public int Id { get; set; }
+    public string Model { get; set; }
+    public string Marka { get; set; }
+    public int Year { get; set; }
+
+    public override string ToString()
+    => $"{Marka}-{Model}-{Year}";
+}
+public class CarGallery
+{
+    public CarGallery(string name, List<Car> cars)
+    {
+        Name = name;
+        Cars = cars;
+    }
+
+    public string Name { get; set; }
+    public List<Car> Cars { get; set; }
+
+
+
+    public void AddCar(Car car) { 
+    
+        Cars.Add(car);
+    }
+    public void RemoveCar(Car car) {
+        Cars.Remove(car);
+    }
+    public void GetAllCar()
+    {
+        List<Car> cars = null;
+        using FileStream fs = new FileStream("cars.json", FileMode.Open);
+        cars = System.Text.Json.JsonSerializer.Deserialize<List<Car>>(fs);
+        foreach (var car in cars)
+        {
+            Console.WriteLine(car);
+        }
+    }
+    public Car GetById(int id)
+    {
+        foreach (var car in Cars)
+        {
+            if (id == car.Id)
+            {
+                return car;
+            }
+        }
+        throw new NullReferenceException("Id is not found!!");
+
+    }
+}
---
+++ JsonHomework/Program.cs
@@ -0,0 +1,41 @@
+﻿using JsonHomework;
+
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+
+void JsonSerializeMethod(List<Car>cars)
+{
+
+    JsonSerializerOptions op = new JsonSerializerOptions();
+    op.WriteIndented = true;
+    var jsonString = JsonSerializer.Serialize(cars, op);
+    File.WriteAllText("cars.json", jsonString);
+}
+void JsonDesiriazlizeMethod()
+{
+    List<Car> cars = null;
+    using FileStream fs = new FileStream("cars.json", FileMode.Open);
+    cars = System.Text.Json.JsonSerializer.Deserialize<List<Car>>(fs);
+    foreach (var car in cars)
+    {
+        Console.WriteLine(car);
+    }
+}
+
+
+List<Car> cars = new List<Car> {
+        new Car(1,"Priora","Lada", 2013),
+        new Car(2,"2107", "Lada",2011),
+        new Car(3,"2106","LadA", 1999),
+};
+CarGallery carGallery = new CarGallery("Gallery1", cars);
+Car car1 = new Car(4,"BMW","F10",2020);
+carGallery.AddCar(car1);
+//carGallery.RemoveCar(car1);
+JsonSerializeMethod(cars);
+
+carGallery.GetAllCar();
+
